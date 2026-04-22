2cf0c84980970ca3bacf9b133ae4ff30383d2d08
Sean <tansean.25@ichat.sp.edu.sg>
2026-04-22 11:48:40 +0800

    togethere

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,4 +2,5 @@ print("Quan Biao IS FRIENDS WITH SHGAMINGCORNER")
 
 print ("Samuel is friends with song heng")
 
-print ("i am a proud student of akps")
+print ("i am a proud student of akps") 
+print("akps")
